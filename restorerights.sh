ee6911219cef2a8e63aaf07a5acb42abb6042331
Otso Rajala <ojrajala@gmail.com>
2016-10-25 12:43:36 +0300

    clean up syntax

--- restorerights.sh
+++ restorerights.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-for i in * ; do if [[ -d $i ]] ; then chmod 755 "$i" ; else chmod 664 "$i" ; fi done
+for i in * ; 
+do
+    if [[ -d $i ]] ;
+    then chmod 755 "$i" ;
+    else chmod 664 "$i" ;
+    fi 
+done
