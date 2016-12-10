3b7b0e6445484970cd63c6890bbd456cde6e5c62
Otso Rajala <ojrajala@gmail.com>
2016-12-10 14:36:24 +0200

    install most relevant gstreamer plugins

---
+++ fedora-gstreamer-plugins-install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+dnf install gstreamer-plugins-{bad,bad-free,bad-free-extras,bad-nonfree,base,good,good-extras,ugly} gstreamer1-plugins-{bad-free,bad-free-extras,bad-freeworld,bad-nonfree,base,good,good-extras,ugly}
