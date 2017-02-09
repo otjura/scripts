c5fabcbe63420fc0c1694d5d07ac22fce79fb802
Otso Rajala <ojrajala@gmail.com>
2017-02-09 12:49:53 +0200

    small impros and more accurate rename

--- ffmpeg-mp3-ruttaaja.sh
+++ ffmpeg-mp3-ruttaaja.sh
@@ -3,15 +3,17 @@
 for file in *;
 do
   if [[ -f "$file" && "${file:${#file}-4}" = ".mp3" ]]; then
-  name=$(basename "$file");
-    ffmpeg -v 24 -i "$name" -codec:a libmp3lame -qscale:a 5 "_q5_$name";
+    name=$(basename "$file");
+    echo "Ruttaa paraikaa tätä: $name";
+    ffmpeg -v 16 -i "$name" -codec:a libmp3lame -qscale:a 5 "_q5_$name";
     rm "$file";
   fi
 done
+echo 'Nimeää väliaikastiedostot takas alkuperäsiks...'
 for qxfile in *;
 do
   if [[ -f "$qxfile" ]]; then
-  qxfilename=$(basename "$qxfile");
+    qxfilename=$(basename "$qxfile");
     mv "$qxfilename" "${qxfilename#_q5_}";
   fi
 done
