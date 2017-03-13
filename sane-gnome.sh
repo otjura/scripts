57eee743260026562d368368bf4ea2821a7ac15a
Otso Rajala <ojrajala@gmail.com>
2017-03-13 18:49:04 +0200

    smaller toolbar icons

--- sane-gnome.sh
+++ sane-gnome.sh
@@ -3,11 +3,11 @@ gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace 9'
 gsettings set org.gnome.desktop.interface font-name 'Cantarell 9'
 gsettings set org.gnome.desktop.interface document-font-name 'Sans 9'
 gsettings set org.gnome.desktop.thumbnail-cache maximum-size 1024
-gsettings set org.gnome.nautilus.icon-view thumbnail-size 48
+gsettings set org.gnome.desktop.interface toolbar-icons-size 'small'
 gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false
 gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Cantarell Bold 9'
 gsettings set org.gnome.desktop.wm.preferences focus-new-windows 'strict'
+gsettings set org.gnome.nautilus.icon-view thumbnail-size 48
 gsettings set org.gtk.exampleapp font 'Monospace 9'
-
 
